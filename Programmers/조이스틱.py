4a50412c91a66697a5ada26f47ad89072e5e117d
hyj <hyj1004mu@naver.com>
2021-08-12 23:21:33 +0900

    Programmers] 조이스틱 풀이

---
+++ Programmers/조이스틱.py
@@ -0,0 +1,23 @@
+def solution(name):
+    moves = [min(ord(x)-ord('A'), ord('Z')-ord(x)+1) for x in name]
+    idx = 0
+    answer = 0
+
+    while True:
+        answer = answer + moves[idx]
+        moves[idx] = 0
+
+        if sum(moves) == 0:
+            return answer
+
+        left, right = 1, 1
+        while moves[idx - left] == 0:
+            left += 1
+        while moves[idx + right] == 0:
+            right += 1
+        
+        answer += left if left < right else right
+        idx += -left if left < right else right
+
+
+print(solution("JAN"))
