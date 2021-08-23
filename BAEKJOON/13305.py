2065ea7a9360165675d3b0254d6c942320ec18c8
hyj <hyj1004mu@naver.com>
2021-08-23 23:55:15 +0900

    백준] 주유소 풀이

--- BAEKJOON/13305.py
+++ BAEKJOON/13305.py
@@ -2,3 +2,21 @@
 # 주유소
 '''
 
+# city_num = int(input())
+lengths = input().split()
+citys = input().split()
+
+ass =0
+
+cost = int(citys[0]) * int(lengths[0])
+print(int(citys[citys.index(min(citys[:-1]))]))
+# [int(n) for n in lengths[citys.index(min(citys)):-1]]
+# print(lengths[citys.index(min(citys))])
+print(sum([int(n) for n in lengths[citys.index(min(citys))-2:]]))
+
+
+
+
+
+
+# cost += int(citys[citys.index(min(citys))-1]) * sum(int(lengths[citys.index(min(citys))-1:-1]))
