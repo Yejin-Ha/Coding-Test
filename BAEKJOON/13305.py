f95dabad44aad529834de681317f58789220e490
hyj <hyj1004mu@naver.com>
2021-08-25 23:57:30 +0900

    update

--- BAEKJOON/13305.py
+++ BAEKJOON/13305.py
@@ -12,11 +12,7 @@ cost = int(citys[0]) * int(lengths[0])
 print(int(citys[citys.index(min(citys[:-1]))]))
 # [int(n) for n in lengths[citys.index(min(citys)):-1]]
 # print(lengths[citys.index(min(citys))])
-print(sum([int(n) for n in lengths[citys.index(min(citys))-2:]]))
-
-
-
-
+print(sum([int(n) for n in lengths[citys.index(min(citys))-2:]]
 
 
 # cost += int(citys[citys.index(min(citys))-1]) * sum(int(lengths[citys.index(min(citys))-1:-1]))
