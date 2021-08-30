aa5a87da9458857b63a578a079014131676d6197
hyj <hyj1004mu@naver.com>
2021-08-31 00:32:14 +0900

    BAEKJOON] 주유소 풀이

--- BAEKJOON/13305.py
+++ BAEKJOON/13305.py
@@ -1,18 +1,27 @@
 '''
 # 주유소
 '''
-
-# city_num = int(input())
+city_num = int(input()) - 2
 lengths = input().split()
 citys = input().split()
+result = 0
 
-ass =0
+cost = int(citys.pop(0))
+street = int(lengths.pop(0))
 
-cost = int(citys[0]) * int(lengths[0])
-print(int(citys[citys.index(min(citys[:-1]))]))
-# [int(n) for n in lengths[citys.index(min(citys)):-1]]
-# print(lengths[citys.index(min(citys))])
-print(sum([int(n) for n in lengths[citys.index(min(citys))-2:]]
+# 맨 마지막 주유소와 시작 주유소는 비교할 필요가 없어서 2를 뺀다.
+while city_num:
+    # 다음 주유소가 싸다 => 멈추기
+    if cost > int(citys[0]):
+        result += cost * street
+        cost = int(citys.pop(0))
+        street = int(lengths.pop(0))
+    # 다음 주유소가 비싸다 => 계속 진행
+    else:
+        citys.pop(0)
+        street += int(lengths.pop(0))
 
+    city_num -= 1
 
-# cost += int(citys[citys.index(min(citys))-1]) * sum(int(lengths[citys.index(min(citys))-1:-1]))
+result += cost * street
+print(result)
