8c1cbb7f36766f5ebd63a622363c8f12c7f30c3a
YeaJin Ha <hyj1004mu@naver.com>
2021-11-22 23:58:24 +0900

    BAEKJOON] 2884 알람 계산 풀이

---
+++ BAEKJOON/2884_알람계산.py
@@ -0,0 +1,13 @@
+times = input()
+times = list(map(int, times.split(' ')))
+
+if times[1] < 45:
+    times[0] -= 1
+    times[1] += 15
+else:
+    times[1] -= 45
+
+if times[0] < 0:
+    times[0] = 24 + times[0]
+
+print(times)
