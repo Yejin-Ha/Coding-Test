2dd902015788a133e21206cbd0249ad24ce542c3
YeaJin Ha <hyj1004mu@naver.com>
2021-11-28 23:28:22 +0900

    BAEKJOON] A + B - 3 풀이

--- BAEKJOON/10950_A+B-3.py
+++ BAEKJOON/10950_A+B-3.py
@@ -1,2 +1,9 @@
 n = int(input())
-for 
+cases = []
+
+for i in range(0, n):
+    a = list(map(int, input().split(' ')))
+    cases.append(a)
+
+for i in range(0, n):
+    print(cases[i][0]+cases[i][1])
