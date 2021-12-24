2bf6c219181c1b7e84981304ab48be4d8ec4270f
하예진 <hyj1004mu@naver.com>
2021-12-24 10:18:53 +0900

    Update 15552_빠른A+B.py

--- BAEKJOON/15552_빠른A+B.py
+++ BAEKJOON/15552_빠른A+B.py
@@ -1,1 +1,16 @@
-a = sys.stdin.readline()
+# sys.stdin.readline()을 사용하기위해 모듈 import
+import sys
+
+n = int(input())
+
+# 두 수를 더한 값들을 담을 배열
+result = []
+
+for i in range(n):
+  # \n을 기준으로 문자열을 구분한다.
+  nums = sys.stdin.readline().strip()
+  a, b = map(int, nums.split(' '))
+  result.append(a+b)
+
+for i in result:
+  print(i)
