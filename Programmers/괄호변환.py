67c99a9a7d83e49e3a732709c48acd4afe965d7a
하예진 <hyj1004mu@naver.com>
2022-01-13 13:25:23 +0900

    Update 괄호변환.py

--- Programmers/괄호변환.py
+++ Programmers/괄호변환.py
@@ -14,24 +14,62 @@
   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
   4-5. 생성된 문자열을 반환합니다.
 '''
-# 입력받은 문자열을 u, v로 구분하는 함수(step 2)
-def divid(p):
-    left, right = 0, 0
-    for i in range(len(p)):
-        if p[i] == '(':
-            left += 1
-        else:
-            right += 1
-        if left == right:
-            u = p[0:i]
-            v = p[i+1:] if i == len(p) else ""
-            break
-    return u, v
-        
-# "올바른 괄호 문자열"인지 확인하는 함수. stack 구조 사용(LIFO)
-def correct_str(p):
-    stack = []
-    stack.append('(')
+# 문자열 w를 u, v로 분리하는 함수
+def divide(w):
+    openP = 0
+    closeP = 0
     
-def solution(p):
-    return answer
+    for i in range(len(w)):
+        if w[i] == '(':
+            openP += 1
+        else:
+            closeP += 1
+        if openP == closeP:
+            return w[:i + 1], w[i + 1:]
+ 
+ 
+# 문자열 u가 올바른 괄호 문자열인지 확인하는 함수
+def isBalanced(u):
+    stack = []
+    
+    for p in u:
+        if p == '(':
+            stack.append(p)
+        else:
+            if not stack:
+                return False
+            stack.pop()
+            
+    return True
+ 
+ 
+def solution(w):
+    # 과정 1
+    if not w:
+        return ""
+    
+    # 과정 2
+    u, v = divide(w)
+    
+    # 과정 3
+    if isBalanced(u):
+        # 과정 3-1
+        return u + solution(v)
+    # 과정 4
+    else:
+        # 과정 4-1
+        answer = '('
+        # 과정 4-2
+        answer += solution(v)
+        # 과정 4-3
+        answer += ')'
+        
+        # 과정 4-4
+        for p in u[1:len(u) - 1]:
+            if p == '(':
+                answer += ')'
+            else:
+                answer += '('
+        
+        # 과정 4-5
+        return answer
